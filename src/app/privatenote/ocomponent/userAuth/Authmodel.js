2066b484ba9343a2f356fd152912b0c634ff2dd5
bikramdhami0011 <bikramdhami296@gmail.com>
2023-09-11 12:25:45 +0545

    hell

--- src/app/privatenote/ocomponent/userAuth/Authmodel.js
+++ src/app/privatenote/ocomponent/userAuth/Authmodel.js
@@ -1,11 +1,7 @@
-import { Colorpass } from "../../prihome/pricomponent/page";
 
 const { default: mongoose } = require("mongoose");
- function Color() {
-        console.log(Colorpass);
-}
 
-Color();
+
 const Userauthschema=new mongoose.Schema({name:{type:String,required:true},
 
     collage:{type:String,required:true},  email:{type:String ,required:true,unique:true},password:{type:String,required:true}  });
